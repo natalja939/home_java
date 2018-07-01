eb1afb5dff06e2f99f8e0da6a0f5acf63abf0276
NataliiaB <bejbanatalja939@gmail.com>
2018-07-01 21:52:50 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const result = solveQuadr(a,b,c);
+alert(result);
+
+
+
+
+const a = parseFloat(prompt('Enter A'));
+const b = parseFloat(prompt('Enter B'));
+const c = parseFloat(prompt('Enter C'));
+function solveQuadr(a,b,c) {
+    const d = calcD(a,b,c);
+    if(d>0) {
+        
+        return
+        
+    }
+}
+function calcD(a,b,c) {
+    return
+}
