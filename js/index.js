7296cfeaa914f15e0c57d556e9376ff14e7b44d0
NataliiaB <bejbanatalja939@gmail.com>
2018-07-02 13:49:30 +0000

    Edit js

--- js/index.js
+++ js/index.js
@@ -7,18 +7,15 @@ alert(result);
 
 function solveQuadr(a,b,c) {
     const d = calcD(a,b,c);
-    let x1, x2;
     if (d > 0) {
-       x1 = (-b + Math.sqrt(d)) / (2*a);
-       x2 = (-b - Math.sqrt(d)) / (2*a);
+      const x1 = (-b + Math.sqrt(d)) / (2*a);
+      const x2 = (-b - Math.sqrt(d)) / (2*a);
     return 'x1 =' + x1 + ', x2 = ' + x2;
     } else if (d == 0) {
-        x1 = -b / (2*a);
+      const  x1 = -b / (2*a);
         return 'x1 =' + x1;
     } 
-    else if (d < 0) {
     return 'There is no valid roots';
-    }
 }
 function calcD(a,b,c) {
     return b * b - 4 * a * c;
