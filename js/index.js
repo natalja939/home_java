04e07046468cd336739773dade1cf55a7fbe6e43
NataliiaB <bejbanatalja939@gmail.com>
2018-07-02 11:12:56 +0000

    Edit JS

--- js/index.js
+++ js/index.js
@@ -1,20 +1,26 @@
-const result = solveQuadr(a,b,c);
-alert(result);
-
-
-
-
 const a = parseFloat(prompt('Enter A'));
 const b = parseFloat(prompt('Enter B'));
 const c = parseFloat(prompt('Enter C'));
+
+const result = solveQuadr(a,b,c);
+alert(result);
+
 function solveQuadr(a,b,c) {
     const d = calcD(a,b,c);
+    
     if(d>0) {
-        
-        return
+        const x1 = (- b - Math.sqrt(d))/(2 * a);
+        const x2 = (- b + Math.sqrt(d))/(2 * a);
+        return 'x1 = ' + x1,' x2= ' + x2;
+    }
+    if(d==0) {
+        x = -b / 2 * a;
+        return 'x = ' + x;
+    }
+    if(d<0) {
         
     }
 }
 function calcD(a,b,c) {
-    return
+    return b * b - 4 * a * c;
 }
